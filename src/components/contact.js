cdbc4559601d47f2fdca2b511f3bd89c856d6d27
Ezequiel Espinoza <ezequielnoza@gmail.com>
2019-11-06 10:40:42 -0500

    added navigation bar at bottom

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => (
     <section className="contact-section" id="c">
-        <h2>Contact</h2>
+        <h2>Contact Us</h2>
         <a className="social-link" href="https://www.google.com/maps/place/Mount+Rainier+Bike+Co-op/@38.9387985,-76.9605424,17z/data=!4m13!1m7!3m6!1s0x89b7c70cabaee009:0x6686ba9d853ad5cb!2s3601+Bunker+Hill+Rd,+Mt+Rainier,+MD+20712!3b1!8m2!3d38.9387985!4d-76.9583537!3m4!1s0x89b7c70cba911ec9:0xb5ed8b3dc9b8f31a!8m2!3d38.9388006!4d-76.958357" target="_blank" rel="noopener noreferrer">3601 Bunker Hill Rd, Mt Rainier MD 20712</a>
         <a className="social-link" href="https://www.facebook.com/mountrainiermdbikecoop" target="_blank" rel="noopener noreferrer">Facebook</a>
         <a className="social-link" href="https://instagram.com/mountrainierbikecoop" target="_blank" rel="noopener noreferrer">Instagram</a>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 const d = new Date();
 
 const currentYear = d.getFullYear();
 const Footer = () => (
     <footer>
+        <ul className="navigation-links">
+          <li><NavLink to="/bikes">Bikes for Sale</NavLink></li>
+          <li><NavLink to="/#ue">Upcoming Events</NavLink></li>
+          <li><NavLink to="/#c">Contact Us</NavLink></li>
+          <li><Link to="/volunteers">Meet Our Core Volunteers</Link></li>
+        </ul>
         <a href="https://www.ezequielespinoza.dev/">© {currentYear}, Created by Ezequiel Espinoza</a>
     </footer>
 )
