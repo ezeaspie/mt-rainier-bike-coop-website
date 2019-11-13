6096ab6867b46d9363967de560d5a776443d364c
Ezequiel Espinoza <ezequielnoza@gmail.com>
2019-11-13 14:11:31 -0500

    changed community events tab to carousel format

--- src/components/CEvents.js
+++ src/components/CEvents.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import makeCarousel from 'react-reveal/makeCarousel';
+import Slide from 'react-reveal/Slide';
 
 const CEvents = () => {
 
@@ -20,6 +22,23 @@ const CEvents = () => {
     if(!calledServer){
       setTimeout(getCalenderData,50);
     }
+    const CarouselUI = ({ position, total, handleClick, children }) => (
+      <div className="carousel-container">
+        <div className="carousel-children">
+          {children}
+          <div className="arrow left" onClick={handleClick} data-position={position - 1}>{'<'}</div>
+          <div className="arrow right" onClick={handleClick} data-position={position + 1}>{'>'}</div>
+        </div>
+        <span className="container-dots">
+          {Array(...Array(total)).map( (val, index) =>
+            <span className="container-dot" key={index} onClick={handleClick} data-position={index}>
+              {index === position ? '● ' : '○ ' }
+            </span>
+          )}
+        </span>
+      </div>
+    );
+    const Carousel = makeCarousel(CarouselUI);
     return (
       <div className="events-main">
               {
@@ -36,18 +55,20 @@ const CEvents = () => {
                     eventData.map((event, i) => (
                       <div className="cevent">
                         <h3 className="cevent-name">{event.name}</h3>
-                        <div className="cevent-image-list">
+                        <Carousel className="cevent-image-list">
                           {
                             event.images.map((image) => {
                               return(
-                              <div className="cevent-image-container">
+                                <Slide right>
+                                <div className="cevent-image-container">
                                 <img className="cevent-image" src={image.image} alt={image.description}/>
                                 <p className="cevent-image-caption">{image.description}</p>
-                              </div>
+                                </div>
+                                </Slide>
                               )
                             })
                           }
-                        </div>
+                        </Carousel>
                         <div 
                           dangerouslySetInnerHTML={{__html: event.description}}
                           className="cevent description"
