93492430298e8d6534823d56fe66f083a153c08e
Liowan <31571109+Liowan@users.noreply.github.com>
2019-11-14 10:01:03 -0500

    Replaced "cors" on line# 12 - Homepage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = (props) => {
     let requestURL = `https://mt-rainier-bike-coop-backend.herokuapp.com/` // In development? Use : http://localhost:8000/`;
     const getCalenderData = () => {
       fetch(requestURL, {
-        mode:"",
+        mode:"cors",
       })
       .then(res => res.json())
       .then(res =>{
