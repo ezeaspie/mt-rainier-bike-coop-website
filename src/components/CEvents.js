0565781a948ef4580eb606abc7bf73f0a32920b9
Ezequiel Espinoza <ezequielnoza@gmail.com>
2019-11-19 16:01:19 -0500

    fixed a css issue that prevented events showing

--- src/components/CEvents.js
+++ src/components/CEvents.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import makeCarousel from 'react-reveal/makeCarousel';
-import Slide from 'react-reveal/Slide';
 import Slider from "react-slick";
 
 const CEvents = () => {
@@ -22,34 +20,16 @@ const CEvents = () => {
     let [eventData, setEventData] = useState([]);
     if(!calledServer){
       setTimeout(getCalenderData,50);
+      
     }
     const settings = {
       className: "",
-      dots: true,
+      dots: false,
       infinite: true,
-      lazyLoad: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       adaptiveHeight: true
     };
-
-    const CarouselUI = ({ position, total, handleClick, children }) => (
-      <div className="carousel-container">
-        <div className="carousel-children">
-          {children}
-          <div className="arrow left" onClick={handleClick} data-position={position - 1}>{'<'}</div>
-          <div className="arrow right" onClick={handleClick} data-position={position + 1}>{'>'}</div>
-        </div>
-        <span className="container-dots">
-          {Array(...Array(total)).map( (val, index) =>
-            <span className="container-dot" key={index} onClick={handleClick} data-position={index}>
-              {index === position ? '● ' : '○ ' }
-            </span>
-          )}
-        </span>
-      </div>
-    );
-    const Carousel = makeCarousel(CarouselUI);
     return (
       <div className="events-main">
               {
@@ -64,7 +44,7 @@ const CEvents = () => {
                 <div className="cevents-list">
                 {
                     eventData.map((event, i) => (
-                      <div className="cevent">
+                      <div className="cevent" key={i}>
                         <h3 className="cevent-name">{event.name}</h3>
                         <div 
                           dangerouslySetInnerHTML={{__html: event.description}}
@@ -74,7 +54,7 @@ const CEvents = () => {
                           {
                             event.images.map((image) => {
                               return(
-                                <div right>
+                                <div key={image.image}>
                                 <div className="cevent-image-container">
                                 <img className="cevent-image" src={image.image} alt={image.description}/>
                                 <p className="cevent-image-caption">{image.description}</p>
